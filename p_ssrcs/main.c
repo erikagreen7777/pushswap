db52f6f1311e84115ca4e92dc6dce781b2becdaa
erikagreen7777 <erikagreen7777@gmail.com>
2018-02-16 14:11:00 -0800

    same for pushswap

--- p_ssrcs/main.c
+++ p_ssrcs/main.c
@@ -5,18 +5,42 @@ int main(int argc, char **argv)
 	int	temp[argc - 1];
 	char **str;
 	int i;
+	int j;
 	
 	str = NULL;
-	i = 0;
+	j = 0;
 	if (argc > 0)
 		;
-
-	str = ft_strsplit(argv[1], ' ');
-	while (str[i])
+	printf("argc: %d\n", argc);
+	if (argc == 2)
 	{
-		temp[i] = ft_atoi(str[i]);
-		printf("temp[%d]: %d\n", i, temp[i]);
-		i++;
+		str = ft_strsplit(argv[1], ' ');
+		i = 0;
+		while (str[i])
+		{	
+			temp[i] = ft_atoi(str[i]);
+			printf("temp[%d]: %d\n", i, temp[i]);
+			i++;
+		}
 	}
+	else if (argc > 2)
+	{
+		i = 1;
+		while (i < argc)
+		{
+			temp[j] = ft_atoi(argv[i]);
+			j++;
+			i++;
+		}
+		i = 0;
+		while (i < argc - 1)
+		{
+			printf("temp[%d]: %d\n", i, temp[i]);
+			i++;
+		}
+	}
+
+
+
 	return (0);
 }
